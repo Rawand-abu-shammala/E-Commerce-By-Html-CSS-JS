9fbf41edc7ba524ddab54a1607cb9461506c77c0
Rawand-abu-shammala <rwndsami@gmail.com>
2024-03-16 14:01:40 +0200

    custom js link

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
- 
+ 'use script';
+
+//  modal variables 
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overly]');
+
+// modal function
+const modalCloseFunc = function () {
+    modal.classList.add('closed')
+}
+
+// modal eventListener 
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc)
